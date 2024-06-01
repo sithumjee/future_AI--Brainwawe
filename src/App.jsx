c44d1d5e84829003f722c32cdd04039fa45b8ce7
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-06-01 12:41:56 +0530

    first initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
